fa6cca363ec9dd5889ddd83236ff2d126d78ea57
John O'Reilly <oreillyj@gmail.com>
2018-12-28 10:51:57 +0000

    revert to main endpoint

--- base/src/main/java/com/surrus/galwaybus/di/koin/AppModule.kt
+++ base/src/main/java/com/surrus/galwaybus/di/koin/AppModule.kt
@@ -38,8 +38,8 @@ import java.util.concurrent.TimeUnit
 
 
 object DatasourceProperties {
-    //const val SERVER_URL = "https://galwaybus.herokuapp.com/"
-    const val SERVER_URL = "https://api.galwaybusabu.com/"
+    const val SERVER_URL = "https://galwaybus.herokuapp.com/"
+    //const val SERVER_URL = "https://api.galwaybusabu.com/"
 }
 
 
--- base/src/main/java/com/surrus/galwaybus/remote/GalwayBusService.kt
+++ base/src/main/java/com/surrus/galwaybus/remote/GalwayBusService.kt
@@ -16,26 +16,26 @@ import retrofit2.http.Query
 
 interface GalwayBusService {
 
-    @GET("/v1//routes.json")
+    @GET("/routes.json")
     fun getBusRoutes(): Deferred<Response<LinkedHashMap<String, BusRoute>>>
 
-    @GET("/v1//routes/{route_id}.json")
+    @GET("/routes/{route_id}.json")
     fun getStops(@Path("route_id") routeId: String) : Deferred<Response<GetStopsResponse>>
 
-    @GET("/v1//stops.json")
+    @GET("/stops.json")
     fun getAllStops() : Deferred<Response<List<BusStop>>>
 
-    @GET("/v1//stops/nearby.json")
+    @GET("/stops/nearby.json")
     fun getNearestStops(@Query("latitude") latitude: Double, @Query("longitude") longitude: Double) : Deferred<Response<List<BusStop>>>
 
-    @GET("/v1//stops/{stop_ref}.json")
+    @GET("/stops/{stop_ref}.json")
     fun getDepartures(@Path("stop_ref") stopRef: String) : Deferred<Response<GetDeparturesResponse>>
 
-    @GET("/v1//schedules.json")
+    @GET("/schedules.json")
     fun getSchedules() : Deferred<Response<LinkedHashMap<String, List<Map<String, String>>>>>
 
 
-    @GET("/v1//bus/{route_id}.json")
+    @GET("/bus/{route_id}.json")
     fun getBusListForRoute(@Path("route_id") routeId: String) : Deferred<Response<GetBusListForRouteResponse>>
 
 
