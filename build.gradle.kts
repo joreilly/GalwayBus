03a388f4adf8e7bd5d3c772b158b49c230b66efa
John O'Reilly <oreillyj@gmail.com>
2020-10-02 20:56:03 +0100

    AS 4.2 alpha 13

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:4.2.0-alpha12")
+        classpath("com.android.tools.build:gradle:4.2.0-alpha13")
         classpath(BuildPlugins.kotlinGradlePlugin)
         classpath("org.jetbrains.kotlin:kotlin-serialization:${Versions.kotlin}")
         classpath("com.google.gms:google-services:4.3.3")
