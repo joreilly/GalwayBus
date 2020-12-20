8461303a6eb70ed2a56fac22b61c2d9df2f3c055
John O'Reilly <oreillyj@gmail.com>
2020-12-20 15:25:16 +0000

    use ProvideTextStyle

--- android-app/src/main/java/dev/johnoreilly/galwaybus/ui/screens/BusStopDeparturesScreen.kt
+++ android-app/src/main/java/dev/johnoreilly/galwaybus/ui/screens/BusStopDeparturesScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.preferredWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ProvideTextStyle
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -19,21 +21,23 @@ import dev.johnoreilly.galwaybus.ui.utils.quantityStringResource
 
 @Composable
 fun BusStopDeparture(departure: GalwayBusDeparture) {
-    Row(verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp).fillMaxWidth()) {
+    ProvideTextStyle(typography.subtitle1) {
+        Row(verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp).fillMaxWidth()) {
 
-        Text(departure.timetableId, fontWeight = FontWeight.Bold,
-                modifier = Modifier.preferredWidth(36.dp), style = typography.subtitle1)
+            Text(departure.timetableId, fontWeight = FontWeight.Bold,
+                    modifier = Modifier.preferredWidth(36.dp))
 
-        Text(departure.displayName, maxLines = 1, overflow = TextOverflow.Ellipsis,
-                modifier = Modifier.weight(1f).padding(start = 16.dp), style = typography.subtitle1)
+            Text(departure.displayName, maxLines = 1, overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier.weight(1f).padding(start = 16.dp))
 
 
-        val minutesUntilDeparture = departure.durationUntilDeparture.inMinutes.toInt()
-        val departureText = if (minutesUntilDeparture <= 0)
-            stringResource(R.string.bus_time_due)
-        else
-            "$minutesUntilDeparture ${quantityStringResource(R.plurals.mins, minutesUntilDeparture)}"
-        Text(departureText, modifier = Modifier.padding(start = 16.dp), style = typography.subtitle1)
+            val minutesUntilDeparture = departure.durationUntilDeparture.inMinutes.toInt()
+            val departureText = if (minutesUntilDeparture <= 0)
+                stringResource(R.string.bus_time_due)
+            else
+                "$minutesUntilDeparture ${quantityStringResource(R.plurals.mins, minutesUntilDeparture)}"
+            Text(departureText, modifier = Modifier.padding(start = 16.dp))
+        }
     }
 }
