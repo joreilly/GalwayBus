5cb6065e16418269debaced03497866bd466bbde
John O'Reilly <oreillyj@gmail.com>
2020-12-22 09:22:54 +0000

    update to ktor 1.5.0

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -2,8 +2,8 @@
 object Versions {
     const val kotlin = "1.4.21"
     const val kotlinCoroutines = "1.4.2-native-mt"
-    const val ktor = "1.4.0"
-    const val kotlinxSerialization = "1.0.0-RC"
+    const val ktor = "1.5.0"
+    const val kotlinxSerialization = "1.0.1"
     const val kotlinxDateTime = "0.1.1"
     const val koin = "3.0.0-alpha-4"
     const val sqlDelight = "1.4.2"
