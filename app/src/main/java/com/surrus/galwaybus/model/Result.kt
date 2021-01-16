83b78726e8f51b410b896ee27f573d2dcad944cb
John O'Reilly <oreillyj@gmail.com>
2021-01-16 13:07:46 +0000

    remove legacy app module

--- app/src/main/java/com/surrus/galwaybus/model/Result.kt
+++
@@ -1,7 +0,0 @@
-package com.surrus.galwaybus.model
-
-sealed class Result<out T: Any> {
-    data class Success<out T : Any>(val data: T) : Result<T>()
-    data class Error(val exception: Exception) : Result<Nothing>()
-}
-
--- app/src/main/java/com/surrus/galwaybus/ui/data/ResourceState.kt
+++
@@ -1,8 +0,0 @@
-package com.surrus.galwaybus.ui.data
-
-/**
- * Represents the state in which a [Resource] is currently in
- */
-enum class ResourceState {
-    LOADING, SUCCESS, ERROR
-}
