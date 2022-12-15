ee6951f728b26cc473a33635731ff383f84cf28f
John O'Reilly <oreillyj@gmail.com>
2022-12-15 19:41:28 +0000

    update koin/compose

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -8,12 +8,14 @@ object Versions {
     const val kotlinCoroutines = "1.6.4"
     const val kotlinxSerialization = "1.3.3"
     const val kotlinxDateTime = "0.4.0"
-    const val koin = "3.2.1"
+    const val koinCore = "3.3.0"
+    const val koinAndroid = "3.3.1"
+    const val koinAndroidCompose = "3.4.0"
     const val sqlDelight = "1.5.4"
     const val multiplatformSettings = "1.0.0-alpha01"
     const val kmpNativeCoroutines = "0.13.1"
 
-    const val compose = "1.3.1"
+    const val compose = "1.4.0-alpha03"
     const val composeCompiler = "1.3.2"
     const val navCompose = "2.5.2"
     const val accompanist = "0.26.5-rc"
@@ -29,13 +31,6 @@ object Versions {
     const val robolectric = "3.6.1"
 }
 
-
-object Koin {
-    val core = "io.insert-koin:koin-core:${Versions.koin}"
-    val test = "io.insert-koin:koin-test:${Versions.koin}"
-    val android = "io.insert-koin:koin-android:${Versions.koin}"
-    val compose = "io.insert-koin:koin-androidx-compose:${Versions.koin}"
-}
 
 object Firebase {
     val core = "com.google.firebase:firebase-core:16.0.9"
@@ -81,11 +76,11 @@ object Deps {
     }
 
     object Koin {
-        const val core = "io.insert-koin:koin-core:${Versions.koin}"
-        const val test = "io.insert-koin:koin-test:${Versions.koin}"
-        const val testJUnit4 = "io.insert-koin:koin-test-junit4:${Versions.koin}"
-        const val android = "io.insert-koin:koin-android:${Versions.koin}"
-        const val compose = "io.insert-koin:koin-androidx-compose:${Versions.koin}"
+        const val core = "io.insert-koin:koin-core:${Versions.koinCore}"
+        const val test = "io.insert-koin:koin-test:${Versions.koinCore}"
+        const val testJUnit4 = "io.insert-koin:koin-test-junit4:${Versions.koinCore}"
+        const val android = "io.insert-koin:koin-android:${Versions.koinAndroid}"
+        const val compose = "io.insert-koin:koin-androidx-compose:${Versions.koinAndroidCompose}"
     }
 
     object Ktor {
